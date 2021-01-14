19e7b671bbda1a62bfac6a63b09955a1949eb519
Michael <michaelbrockus@gmail.com>
2021-01-13 23:48:37 -0800

    create main app class

--- code/program.py
+++ code/program.py
@@ -5,6 +5,16 @@
 # author: Michael Brockus
 # gmail: <michaelbrockus@gmail.com>
 #
+from PyQt5.QtWidgets import QApplication
+
+
+class MesonUi(QApplication):
+    def __init__(self, sys_argv):
+        super(self.__class__, self).__init__(sys_argv)
+
+    def boot(self):
+        pass
+
 
 def greet():
     return "Hello, Python Developer"
